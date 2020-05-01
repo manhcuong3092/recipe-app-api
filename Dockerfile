70406c9dc72a1cbc651f3ec5488e523b802c30f0
manhcuong1122 <cuongdinhmanh1122@gmail.com>
2020-05-01 14:43:02 +0700

    Updated admin to support custom user model

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,11 @@ ENV PYTHONUNBUFFERED 1
 RUN mkdir /app
 WORKDIR /app
 COPY requirements.txt /app/
+RUN apk add --update --no-cache postgresql-client
+RUN apk add --update --no-cache --virtual .tmp-build-deps \
+    gcc libc-dev linux-headers postgresql-dev
 RUN pip install -r requirements.txt
+RUN apk del .tmp-build-deps
 COPY . /app/
 
 RUN adduser -D user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,7 +9,7 @@ class ModelTests(TestCase):
         email = 'test@gmail.com'
         password = 'Testpass123'
         user = get_user_model().objects.create_user(
-            email = email,
+            email=email,
             password=password
         )
         self.assertEqual(user.email, email)
@@ -29,7 +29,9 @@ class ModelTests(TestCase):
 
     def test_create_superuser(self):
         """Test creating a new superuser"""
-        user = get_user_model().objects.create_superuser('test@gmail.com', 'test123')
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com', 'test123'
+        )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
